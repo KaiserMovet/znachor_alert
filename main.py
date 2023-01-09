b2f94ac38d6c06a1b6aac3f1c4fa2c007150cf47
Mateusz Rynkiewicz <rynkiewiczmate@gmail.com>
2023-01-09 13:19:50 +0100

    Added argparse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import argparse
 import datetime
 import json
-import sys
 from pprint import pprint
 from typing import List
 
@@ -10,16 +10,16 @@ from app import EPG, Emission, TVParser
 from app.pictures import Pictures
 
 
-def get_wykop() -> wykop.WykopAPI:
+def get_wykop(app_key, secret_key, account_key) -> wykop.WykopAPI:
     api = wykop.WykopAPI(
-        sys.argv[1],
-        sys.argv[2],
+        app_key,
+        secret_key,
     )
-    api.authenticate(sys.argv[3])
+    api.authenticate(account_key)
     return api
 
 
-def get_emmissions():
+def get_emmissions() -> list[Emission]:
     tv_parser = TVParser(EPG().tree)
     return tv_parser.get_all_elements("Znachor")
 
@@ -84,28 +84,63 @@ def add_emissions_to_history(emissions: List[Emission]) -> None:
         f.write(json.dumps(full_data, indent=4))
 
 
-def get_counter() -> int:
+def get_counter(increment=True) -> int:
     with open("history.json", "r") as f:
         full_data = json.load(f)
     counter = full_data.get("counter", 0)
-    counter += 1
-    full_data["counter"] = counter
-    with open("history.json", "w") as f:
-        f.write(json.dumps(full_data, indent=4))
+    if increment:
+        counter += 1
+        full_data["counter"] = counter
+        with open("history.json", "w") as f:
+            f.write(json.dumps(full_data, indent=4))
     return counter
 
 
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description=(
+            f"Script will download data from {EPG.URL} "
+            "and post entry on Wykop.pl"
+        )
+    )
+    parser.add_argument(
+        "--demo",
+        help="Entry will be generated, but not published on Wykop.pl",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--app-key",
+        help="Wykop App key. Can be ignore, if --demo is set",
+    )
+    parser.add_argument(
+        "--secret-key",
+        help="Wykop secret key. Can be ignore, if --demo is set",
+    )
+    parser.add_argument(
+        "--account-key",
+        help="Wykop user account key. Can be ignore, if --demo is set",
+    )
+    args = parser.parse_args()
+    return args
+
+
 def main() -> None:
+    args = get_args()
     em = get_emmissions()
     pprint(em)
     future_em = [e for e in em if not e.already_took_place()]
     add_emissions_to_history(em)
     if future_em:
-        counter = get_counter()
+        counter = get_counter(increment=not args.demo)
         msg = generate_wykop_entry(future_em, counter)
-        api = get_wykop()
-        add_wykop_entry(api, msg)
         print(msg)
+        if not args.demo:
+            api = get_wykop(
+                app_key=args.app_key,
+                secret_key=args.secret_key,
+                account_key=args.account_key,
+            )
+            add_wykop_entry(api, msg)
     else:
         print("No future emmissions")
 
